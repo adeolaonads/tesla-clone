b6419626a77e45064f064c0805d68a5e91c23176
StrokeOfGeniusX <adeolaonads@gmail.com>
2021-11-03 15:20:54 +0100

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,76 @@
 import React from 'react'
+import styled from 'styled-components'
+import MenuIcon from '@material-ui/icons/Menu'
 
 function Header() {
     return (
-        <div>
-        </div>
-    )
+      <Container>
+        <a href="#">
+          <img src="images/logo.svg" alt="" />
+        </a>
+        <Menu>
+            <a href="#">Model S</a>
+            <a href="#">Model 3</a>
+            <a href="#">Model X</a>
+            <a href="#">Model Y</a>
+        </Menu>
+
+        <RightMenu>
+          <a href="#">Shop</a>
+          <a href="#">Tesla Account</a>
+
+        <CustomMenu />
+
+        </RightMenu>
+      </Container>
+    );
 }
 
 export default Header
+
+
+const Container = styled.div`
+  display: flex;
+  min-height: 60px;
+  position: fixed;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+
+const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+
+    a {
+        font-weight: 600;
+        text-transform: uppercase;
+        padding: 0 10px;
+        flex-wrap: no-wrap;
+    }
+
+    @media(max-width: 768px) {
+      display: none;
+    }
+`
+
+
+const RightMenu = styled.div`
+a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin: 0;
+    padding: 0;
+    
+  }
+`
+
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,47 +1,59 @@
 import React from 'react'
 import styled from 'styled-components'
 
-function Section() {
-    return (
-      <Wrap>
-        <itemText>
-          <h1>Model S</h1>
-          <p>Order Online for Touchless Delivery</p>
-        </itemText>
-        <Buttons>
-          <ButtonGroup>
-            <LeftButton>Custom Order</LeftButton>
-            <RightButton>Existing Inventory</RightButton>
-          </ButtonGroup>
-          <DownArrow src="/images/down-arrow.svg" />
-        </Buttons>
-      </Wrap>
-    );
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
+  return (
+    <Wrap bgImage={backgroundImg}>
+      <itemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </itemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
+    </Wrap>
+  );
 }
 
 export default Section
 
 
 const Wrap = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-image: url('/images/model-s.jpg');
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between; // vertical alignment
-    align-items: center;
-    
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: ${props => `url("/images/${props.bgImage}")`};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; // vertical alignment
+  align-items: center; // horizontal
+  padding-top: 100px;
 `
+
 const itemText = styled.div`
     padding-top: 15vh;
     text-align: center;
+    
 `
 
 const ButtonGroup = styled.div`
     display: flex;
+    margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    } 
 `
 
 const LeftButton = styled.div`
@@ -53,7 +65,7 @@ const LeftButton = styled.div`
     justify-content: center;
     align-items: center;
     border-radius: 100px;
-    opacoty: 0.85;
+    opacity: 0.85;
     text-transform: uppercase;
     font-size: 12px;
     cursor: pointer;
@@ -69,7 +81,6 @@ const RightButton = styled(LeftButton)`
 `
 
 const DownArrow = styled.img`
-  margin-top: 20px;
   height: 40px;
   animation: animateDown infinite 1.5s;
   overflow-x: hidden;
